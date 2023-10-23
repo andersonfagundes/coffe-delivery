bbfb529a6fd6cad7c857ab649b23ed296c8831ec
Anderson <anderson.fagundes4@gmail.com>
2023-10-23 13:53:22 +0100

    Add Router

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route path="/" />
+      </Route>
+    </Routes>
+  )
+}
