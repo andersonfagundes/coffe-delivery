cb5a17dcb5844ed8e23b1ef281653afac9be6c78
Anderson <anderson.fagundes4@gmail.com>
2023-11-30 15:14:17 +0000

    Main section completed

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,53 @@
 import {
   CoffeDeliveryInfo,
   CoffeImageContainer,
+  CoffeInformation,
   CoffeeFinder,
   MainContainer,
   MainInformation,
+  PurchaseAssurance,
 } from './styles'
 import coffeMainImage from '../../assets/coffe-main-image.svg'
+import iconSimpleSafePurchase from '../../assets/icon-simple-and-safe-purchase.svg'
+import iconKeepingIntactCoffe from '../../assets/icon-packeges-keeping-intact-coffee.svg'
+import iconFastAndTrackedDelivery from '../../assets/icon-fast-and-tracked-delivery.svg'
+import iconCoffeArrivesFreshToYou from '../../assets/icon-coffee-arrives-fresh-to-you.svg'
 
 export function Home() {
   return (
     <MainContainer>
       <MainInformation>
-        <CoffeeFinder>
-          Encontre o café perfeito para qualquer hora do dia
-        </CoffeeFinder>
-        <CoffeDeliveryInfo>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </CoffeDeliveryInfo>
+        <CoffeInformation>
+          <CoffeeFinder>
+            Find the perfect coffee for any time of day
+          </CoffeeFinder>
+          <CoffeDeliveryInfo>
+            With Coffee Delivery you receive your coffee wherever you are,
+            anytime hour
+          </CoffeDeliveryInfo>
+          <PurchaseAssurance>
+            <ul>
+              <li>
+                <img src={iconSimpleSafePurchase} alt="" />
+                Simple and safe purchase
+              </li>
+              <li>
+                <img src={iconKeepingIntactCoffe} alt="" />
+                Packaging keeps the coffee intact
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <img src={iconFastAndTrackedDelivery} alt="" />
+                Fast and tracked delivery
+              </li>
+              <li>
+                <img src={iconCoffeArrivesFreshToYou} alt="" />
+                Coffee arrives fresh to you
+              </li>
+            </ul>
+          </PurchaseAssurance>
+        </CoffeInformation>
       </MainInformation>
       <CoffeImageContainer>
         <img src={coffeMainImage} alt="" />
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components'
+import backgroundImageMainInformation from '../../assets/background-main-information.svg'
 
 export const MainContainer = styled.main`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  background-image: url(${backgroundImageMainInformation});
 `
 
 export const MainInformation = styled.div`
   width: 36.75rem;
   height: 12rem;
   margin: 94px 0px 108px 160px;
+`
+export const CoffeInformation = styled.div`
+  width: 36.75rem;
+  height: 21.37rem;
 `
 
 export const CoffeeFinder = styled.div`
@@ -21,15 +27,42 @@ export const CoffeeFinder = styled.div`
 `
 
 export const CoffeDeliveryInfo = styled.div`
+  margin-top: 16px;
   width: 36.75rem;
   height: 3.25rem;
   font-family: 'Roboto', sans-serif;
   font-size: 1.25rem;
   line-height: 130%;
-  margin-top: 16px;
+`
+export const PurchaseAssurance = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-top: 66px;
+  width: 35.43rem;
+  height: 5.25rem;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  line-height: 130%;
+  color: ${(props) => props.theme['base-text']};
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+
+  li {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 5.5px;
+    width: 294px;
+    list-style: none;
+  }
 `
 export const CoffeImageContainer = styled.div`
+  margin: 92px 160px 92px 56px;
   width: 29.75rem;
   height: 22.5rem;
-  margin: 92px 160px 92px 56px;
 `
